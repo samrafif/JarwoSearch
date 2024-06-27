6f991da201a0d3f788d4a375f3a01889bcd1b6f1
Samwoof <abrisam.rm@gmail.com>
2024-06-27 21:59:46 +0700

    ♻️ Refactored frontend serving code

--- backend/endpoints/frontend_handler.py
+++ backend/endpoints/frontend_handler.py
@@ -4,17 +4,28 @@ from flask import (
     Blueprint, send_from_directory, url_for
 )
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+from backend.constants import root_dir, Server
+
+bp = Blueprint("frontend", __name__)
 
 # Path for our main Svelte page
 @bp.route("/")
 def base():
-    root_dir = os.getcwd()
-    return send_from_directory(os.path.join(root_dir, 'frontend', 'public'), 'index.html')
+    print(root_dir)
+
+    return send_from_directory(os.path.join(root_dir, Server.frontend_entrypoint), "index.html")
 
 # Path for all the static files (compiled JS/CSS, etc.)
 @bp.route("/<path:path>")
 def home(path):
-    root_dir = os.getcwd()
-    print(path)
-    return send_from_directory(os.path.join(root_dir, 'frontend/public'), path)
+    
+    return send_from_directory(os.path.join(root_dir, Server.frontend_entrypoint), path)
+
+def checks(app):
+    from os.path import isdir, isfile, join
+
+    checks = [isdir(Server.frontend_entrypoint), isfile(join(Server.frontend_entrypoint, "index.html"))]
+    return all(checks)
+
+def setup(app):
+    app.register_blueprint(bp)
