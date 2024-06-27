5733e74c884e5471a44857f3445722355b8a79c3
Samwoof <abrisam.rm@gmail.com>
2024-06-27 22:01:13 +0700

    ➕ Added frontend deps, flowbite, tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,36 +1,32 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  base: "./", // This will make paths relative
-  build: {
-     emptyOutDir: true,
-     outDir: './public', // Where we want to put the build
-     assetsDir: 'assets', // This will be folder inside the public
-     rollupOptions: {
-        input: {
-           main: './index.html', // This index.html will be in public folder
-           // if you have more pages, just add them bellow like this:
-           // example: './pages/example.html',
-        },
-        output: {
-           entryFileNames: 'assets/js/[name]-[hash].js', // Here we put all js files into js folder
-           chunkFileNames: 'assets/js/[name]-[hash].js',
-           // But after that we need to define which files should go where with regex
-           assetFileNames: ({ name }) => {
-              if (/\.(gif|jpe?g|png|svg|ico)$/.test(name ?? '')) {
-                 return 'assets/images/[name].[ext]';
-              }
+   plugins: [svelte()],
+   base: "./",
+   build: {
+      emptyOutDir: true,
+      outDir: './dist',
+      assetsDir: 'assets',
+      rollupOptions: {
+         input: {
+            main: './index.html',
+         },
+         output: {
+            entryFileNames: 'assets/js/[name]-[hash].js',
+            chunkFileNames: 'assets/js/[name]-[hash].js',
+            assetFileNames: ({ name }) => {
+               if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+                  return 'assets/images/[name].[ext]';
+               }
 
-              if (/\.css$/.test(name ?? '')) {
-                 return 'assets/css/[name]-[hash].[ext]';
-              }
+               if (/\.css$/.test(name ?? '')) {
+                  return 'assets/css/[name]-[hash].[ext]';
+               }
 
-              return 'assets/[name]-[hash].[ext]';
-           },
-        }
-     }     
-  }
+               return 'assets/[name]-[hash].[ext]';
+            },
+         }
+      }     
+   }
 })
